8db7ca114d3b5de8cc012af44e7b017ad1fbeb21
Az <airz.xu@gmail.com>
2019-12-15 23:04:43 +0000

    Initiate project

---
+++ db.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/olivere/elastic"
+)
+
+func createIndexIfNotExist() {
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+	}
+
+	exists, err := client.IndexExists(POST_INDEX).Do(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	if !exists {
+		mapping := `{
+			"mappings": {
+				"properties": {
+					"location": {
+						"type": "geo_point"
+					}
+				}
+			}
+		}`
+		_, err := client.CreateIndex(POST_INDEX).Body(mapping).Do(context.Background())
+		if err != nil {
+			panic(err)
+		}
+	}
+}
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/olivere/elastic"
+)
+
+const (
+	POST_INDEX = "post"
+	ES_URL     = "http://35.239.60.232:9200"
+)
+
+type Location struct {
+	Lat float64 `json:"lat"`
+	Lon float64 `json:"lon"`
+}
+
+type Post struct {
+	// `json:"user"` is for the json parsing of this User field. Otherwise, by default it's 'User'.
+	User     string   `json:"user"`
+	Message  string   `json:"message"`
+	Location Location `json:"location"`
+}
+
+func main() {
+	fmt.Println("started-service")
+	createIndexIfNotExist()
+	http.HandleFunc("/post", handlerPost)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handlerPost(w http.ResponseWriter, r *http.Request) {
+	// Parse from body of request to get a json object.
+	fmt.Println("Received one post request")
+	decoder := json.NewDecoder(r.Body)
+	var p Post
+	if err := decoder.Decode(&p); err != nil {
+		panic(err)
+
+	}
+	fmt.Fprintf(w, "Post received: %s\n", p.Message)
+}
